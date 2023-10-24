9b076d901f73f92f0887a478bac045dda395845d
JaquanAhmed <jaq-ahmed@hotmail.co.uk>
2023-10-24 17:10:15 +0100

    navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,14 @@
 
-const navLinks : string[] = ['About', "Experience", "Projects", "Contacts"];
+const navLinks : string[] = ["About", "Experience", "Projects", "Contacts", "dfjhhgd"];
 
 const Navbar = () => (
     <nav>
         <div>
             <ul className = "list-none m-0 p-0 overflow-hidden bg-slate-800">
-                {navLinks.map((link : string) =>
+                {navLinks.map((link : String) =>
                     (<li className = "float-left">
                         <a 
-                        className = "block text-white text-center p-14 16 decoration-0 hover:text-red" 
+                        className = "block text-white text-center py-4 px-16 decoration-0" 
                         href = "#">{link}
                         </a>
                     </li>)
